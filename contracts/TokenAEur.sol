0d31c9ae2da0210d67a3513d80a7433ac088b012
Peter Petrovics <szerintedmi@gmail.com>
2018-02-22 20:17:39 +0000

    reduce A-EUR decimals to 2 (from 4)

--- contracts/TokenAEur.sol
+++ contracts/TokenAEur.sol
@@ -5,7 +5,7 @@ import "./generic/AugmintToken.sol";
 
 contract TokenAEur is AugmintToken {
     function TokenAEur(address _feeAccount, uint _transferFeePt, uint _transferFeeMin, uint _transferFeeMax)
-    public AugmintToken("Augmint Crypto Euro", "A-EUR", "EUR", 4, _feeAccount,
+    public AugmintToken("Augmint Crypto Euro", "A-EUR", "EUR", 2, _feeAccount,
                                             _transferFeePt, _transferFeeMin, _transferFeeMax )
     {} // solhint-disable-line no-empty-blocks
 
