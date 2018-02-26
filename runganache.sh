d73ea1f8e2648d27749fde358a303df9e762ec66
Peter Petrovics <szerintedmi@gmail.com>
2018-02-26 14:47:30 +0000

    ganache gasprice to 1 GWEI (for UI with Metamask)

--- runganache.sh
+++ runganache.sh
@@ -1,6 +1,6 @@
 echo "launching ganache-cli (aka testrpc) with deterministic addresses" $(npm bin)
 $(npm bin)/ganache-cli \
 --gasLimit 0x47D5DE \
---gasPrice 1 \
+--gasPrice 1000000000 \ # 1 GWEI
 --network-id 999 \
 -m "hello build tongue rack parade express shine salute glare rate spice stock"
