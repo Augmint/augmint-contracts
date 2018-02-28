ee21d38c6533edb2973fb2618daf1f3f48f97867
Peter Petrovics <szerintedmi@gmail.com>
2018-02-28 01:04:36 +0000

    removed unused SignCheck.sol (causing compilation warnings)

--- .solcover.js
+++ .solcover.js
@@ -2,9 +2,5 @@ module.exports = {
     compileCommand: "../node_modules/.bin/truffle compile",
     testCommand: "../node_modules/.bin/truffle test --network coverage",
     // skip interfaces until this resolved: https://github.com/sc-forks/solidity-coverage/issues/162
-    skipFiles: [
-        "interfaces/ERC20Interface.sol",
-        "interfaces/TokenReceiver.sol",
-        "generic/SignCheck.sol"
-    ]
+    skipFiles: ["interfaces/ERC20Interface.sol", "interfaces/TokenReceiver.sol"]
 };
