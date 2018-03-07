6529b5323b32bc7d62c0dd5252ca6cb3dfb8aedc
Peter Petrovics <szerintedmi@gmail.com>
2018-03-07 03:53:13 +0000

    authorise ratesfeeder account to setRates when on rinkeby

--- migrations/3_deploy_Rates.js
+++ migrations/3_deploy_Rates.js
@@ -6,5 +6,8 @@ module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(Rates);
     const rates = Rates.at(Rates.address);
     await rates.grantPermission(accounts[0], "setRate");
+    if (web3.version.network == 4) {
+        await rates.grantPermission("0x8c58187a978979947b88824dcda5cb5fd4410387", "setRate"); // ratesfeeder account
+    }
     await rates.setRate("EUR", 99800);
 };
