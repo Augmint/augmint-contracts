2fa10b4b84712caf9663886ace58b5fa3ceed3a4
Peter Petrovics <szerintedmi@gmail.com>
2018-03-07 12:51:44 +0000

    grant setRate for KP on rinkeby

--- migrations/3_deploy_Rates.js
+++ migrations/3_deploy_Rates.js
@@ -5,9 +5,16 @@ module.exports = async function(deployer, network, accounts) {
     deployer.link(SafeMath, Rates);
     await deployer.deploy(Rates);
     const rates = Rates.at(Rates.address);
-    await rates.grantPermission(accounts[0], "setRate");
+    let grantTxs = [rates.grantPermission(accounts[0], "setRate")];
+
     if (web3.version.network == 4) {
-        await rates.grantPermission("0x8c58187a978979947b88824dcda5cb5fd4410387", "setRate"); // ratesfeeder account
+        console.log("On rinkeby. Granting permissions to setRate for dev and ratesfeeder accounts.");
+        grantTxs = grantTxs.concat([
+            rates.grantPermission("0x8c58187a978979947b88824dcda5cb5fd4410387", "setRate"), // ratesfeeder account
+            rates.grantPermission("0xd3D44BCf1F430edD91781AD5a231b999684c2feB", "setRate") // KP's account
+        ]);
     }
+    await Promise.all(grantTxs);
+
     await rates.setRate("EUR", 99800);
 };
