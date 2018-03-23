0bacae4bf4908d22c61b2b4c5790b65bf10753ae
Peter Petrovics <szerintedmi@gmail.com>
2018-03-23 10:10:42 +0000

    require path fix

--- test/helpers/tokenTestHelpers.js
+++ test/helpers/tokenTestHelpers.js
@@ -1,7 +1,7 @@
 const BigNumber = require("bignumber.js");
 const testHelpers = new require("./testHelpers.js");
 
-const AugmintToken = artifacts.require("./mocks/TokenAEur.sol");
+const AugmintToken = artifacts.require("./TokenAEur.sol");
 const AugmintReserves = artifacts.require("./AugmintReserves.sol");
 const MonetarySupervisor = artifacts.require("./MonetarySupervisor.sol");
 const InterestEarnedAccount = artifacts.require("./InterestEarnedAccount.sol");
