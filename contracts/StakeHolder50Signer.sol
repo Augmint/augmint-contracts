c3ce3c16f80d3d83ccd4dfb8ca4b347e9f02b934
Peter Petrovics <szerintedmi@gmail.com>
2018-04-01 15:35:39 +0200

    50.00001% for quorum

--- contracts/StakeHolder50Signer.sol
+++ contracts/StakeHolder50Signer.sol
@@ -5,7 +5,7 @@ import "./generic/MultiSig.sol";
 
 contract StakeHolder50Signer is MultiSig {
     function StakeHolder50Signer(address[] _signers)
-    public MultiSig(500000, _signers)
+    public MultiSig(500001, _signers)
     {} // solhint-disable-line no-empty-blocks
 
 }
