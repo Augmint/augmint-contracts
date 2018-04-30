69fa1d05291020169a9a1d01c04e88fae3b48a4c
szerintedmi <szerintedmi@gmail.com>
2018-04-30 18:32:37 +0100

    test fixes for new initial local migration data

--- test/locker.js
+++ test/locker.js
@@ -147,10 +147,12 @@ contract("Lock", accounts => {
         // the products should be [ perTermInterest, durationInSecs, maxLockAmount, isActive ] all
         // represented as uints (i.e. BigNumber objects in JS land):
         const [perTermInterest, durationInSecs, minimumLockAmount, maxLockAmount, isActive] = product;
+        const expMaxLockAmount = await monetarySupervisor.getMaxLockAmount(minimumLockAmount, perTermInterest);
+
         assert(perTermInterest.toNumber() === expectedPerTermInterest.toNumber());
         assert(durationInSecs.toNumber() === expectedDurationInSecs.toNumber());
         assert(minimumLockAmount.toNumber() === expectedMinimumLockAmount.toNumber());
-        assert.equal(maxLockAmount.toString(), ltdParams.allowedDifferenceAmount.toString());
+        assert.equal(maxLockAmount.toString(), expMaxLockAmount.toString());
         assert(!!isActive.toNumber() === expectedIsActive);
     });
 
