f546913baa66ae656a70d8985a9a391c363705ac
szerintedmi <szerintedmi@gmail.com>
2018-05-08 15:46:32 +0100

    no need to deploy SafeMath it's part of each contract using it

--- migrations/6_deploy_TokenAEur.js
+++ migrations/6_deploy_TokenAEur.js
@@ -1,10 +1,7 @@
-const SafeMath = artifacts.require("./SafeMath.sol");
 const TokenAEur = artifacts.require("./TokenAEur.sol");
 const FeeAccount = artifacts.require("./FeeAccount.sol");
 const TxDelegator = artifacts.require("./TxDelegator.sol");
 
 module.exports = function(deployer) {
-    deployer.link(SafeMath, TokenAEur);
-
     deployer.deploy(TokenAEur, TxDelegator.address, FeeAccount.address);
 };
