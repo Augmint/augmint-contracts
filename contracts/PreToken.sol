b396cfa4e1bdc198c069bde21fb97790e03f3cff
szerintedmi <szerintedmi@gmail.com>
2018-06-08 00:07:11 +0100

    transfer total agreement ownership only & issue/burn by agreementHash

--- contracts/PreToken.sol
+++ contracts/PreToken.sol
@@ -1,13 +1,15 @@
-/* Augmint pretoken contract to record tokens allocated based on agreements.
+/* Augmint pretoken contract to record agreements and tokens allocated based on the agreement.
 Important: this is NOT an ERC20 token!
 These tokens are not fungible because agreements can have different conditions (valuationCap and discount)
     and pretokens are not tradable.
+Only the total pretoken amount and the aggrement can be transfered to a different account to be able to change wallets
 Despite it some ERC20 functions are implemented so agreement owners can see their
     balances and transfers in standard wallets.
 Restrictions:
-  - only 0 or the total account balance can be transfered
-  - transfer is only allowed by agreement holders (to avoid polluting transfer logs)
-  - transfer is only allowed to accounts without an agreement yet or the same agreement
+  - only total account balance can be transfered - effectively ERC20 transfer used to transfer agreement ownership
+  - only allowed to agreement holders can transfer
+    (i.e. can't transfer 0 amount if have no agreement to avoid polluting logs with Transfer events)
+  - transfer is only allowed to accounts without an agreement yet
   - no approval and transferFrom ERC20 functions
  */
 
@@ -28,16 +30,20 @@ contract PreToken is Restricted {
     uint public totalSupply;
 
     struct Agreement {
+        address owner;
         uint balance;
-        bytes32 agreementHash; // SHA-2 (SHA-256) hash of signed agreement.
-                              // OSX: shasum -a 256 agreement.pdf
-                              // Windows: certUtil -hashfile agreement.pdf SHA256
         uint32 discount; //  discountRate in parts per million , ie. 10,000 = 1%
         uint32 valuationCap; // in USD (no decimals)
     }
 
-    mapping(address => Agreement) public agreements; // Balances for each account
-    address[] public allAgreements; // all addresses with agreements to able to iterate over
+    /* Agreement hash is the SHA-2 (SHA-256) hash of signed agreement document.
+         To generate:
+            OSX: shasum -a 256 agreement.pdf
+            Windows: certUtil -hashfile agreement.pdf SHA256 */
+    mapping(address => bytes32) public agreementOwners; // to lookup agrement by owner
+    mapping(bytes32 => Agreement) public agreements;
+
+    bytes32[] public allAgreements; // all agreements to able to iterate over
 
     event Transfer(address indexed from, address indexed to, uint amount);
 
@@ -45,78 +51,74 @@ contract PreToken is Restricted {
 
     function addAgreement(address owner, bytes32 agreementHash, uint32 discount, uint32 valuationCap)
     external restrict("PreTokenSigner") {
-        require(owner != address(0));
-        require(agreements[owner].agreementHash == 0x0);
-        require(agreementHash != 0x0);
+        require(owner != address(0), "owner must not be 0x0");
+        require(agreementOwners[owner] == 0x0, "owner must not have an aggrement yet");
+        require(agreementHash != 0x0, "agreementHash must not be 0x0");
+        require(discount > 0, "discount must be > 0");
+        require(agreements[agreementHash].discount == 0, "agreement must not exist yet");
 
-        agreements[owner] = Agreement(0, agreementHash, discount, valuationCap);
-        allAgreements.push(owner);
+        agreements[agreementHash] = Agreement(owner, 0, discount, valuationCap);
+        agreementOwners[owner] = agreementHash;
+        allAgreements.push(agreementHash);
 
         emit NewAgreement(owner, agreementHash, discount, valuationCap);
     }
 
-    function issueTo(address _to, uint amount) external restrict("PreTokenSigner") {
-        Agreement storage to = agreements[_to];
-        require(to.agreementHash != 0x0);
+    function issueTo(bytes32 agreementHash, uint amount) external restrict("PreTokenSigner") {
+        Agreement storage agreement = agreements[agreementHash];
+        require(agreement.discount > 0, "agreement must exist");
 
-        to.balance = to.balance.add(amount);
+        agreement.balance = agreement.balance.add(amount);
         totalSupply = totalSupply.add(amount);
 
-        emit Transfer(0x0, _to, amount);
+        emit Transfer(0x0, agreement.owner, amount);
     }
 
     /* Restricted function to allow pretoken signers to fix incorrect issuance */
-    function burnFrom(address from, uint amount)
+    function burnFrom(bytes32 agreementHash, uint amount)
     public restrict("PreTokenSigner") returns (bool) {
-        require(amount > 0, "burn amount must be > 0"); // this effectively restricts burning from agreement holders only
-        require(agreements[from].balance >= amount, "must not burn more than balance"); // .sub would revert anyways but emit reason
+        Agreement storage agreement = agreements[agreementHash];
+        require(agreement.discount > 0, "agreement must exist"); // this is redundant b/c of next requires but be explicit
+        require(amount > 0, "burn amount must be > 0");
+        require(agreement.balance >= amount, "must not burn more than balance"); // .sub would revert anyways but emit reason
 
-        agreements[from].balance = agreements[from].balance.sub(amount);
+        agreement.balance = agreement.balance.sub(amount);
         totalSupply = totalSupply.sub(amount);
 
-        emit Transfer(from, 0x0, amount);
+        emit Transfer(agreement.owner, 0x0, amount);
         return true;
     }
 
-    function balanceOf(address who) public view returns (uint) {
-        return agreements[who].balance;
+    function balanceOf(address owner) public view returns (uint) {
+        return agreements[agreementOwners[owner]].balance;
     }
 
+    /* function to transfer agreement ownership to other wallet by owner
+        it's in ERC20 form so owners can use standard ERC20 wallet just need to pass full balance as value */
     function transfer(address to, uint amount) public returns (bool) { // solhint-disable-line no-simple-event-func-name
         _transfer(msg.sender, to, amount);
         return true;
     }
 
     /* Restricted function to allow pretoken signers to fix if pretoken owner lost keys */
-    function transferFrom(address from, address to, uint amount)
+    function transferAgreement(bytes32 agreementHash, address to)
     public restrict("PreTokenSigner") returns (bool) {
-        _transfer(from, to, amount);
+        _transfer(agreements[agreementHash].owner, to, agreements[agreementHash].balance);
         return true;
     }
 
-    /* private function used by transferFrom & transfer */
+    /* private function used by transferAgreement & transfer */
     function _transfer(address from, address to, uint amount) private {
-        require(agreements[from].agreementHash != 0x0, "only holder of an agreement can transfer");
+        Agreement storage agreement = agreements[agreementOwners[from]];
+        require(agreementOwners[from] != 0x0, "from agreement must exists");
+        require(agreementOwners[to] == 0, "to must not have an agreement");
         require(to != 0x0, "must not transfer to 0x0");
-        require(
-            agreements[to].agreementHash == 0 ||  // allow to transfer to address without agreement
-            agreements[to].agreementHash == agreements[from].agreementHash // allow transfer to acc w/ same agr.
-            , "must transfer to same or 0x0 agreementHash account"
-        );
-        require(
-            amount == 0 || // allow 0 amount transfers to any acc without agreementHash for voting
-            amount == agreements[from].balance // allow only to transfer full balance
-            , "must transfer 0 or full balance"
-        );
+        require(amount == agreement.balance, "must transfer full balance");
 
-        if (amount > 0) { // transfer agreement if it's not a 0 amount "vote only" transfer
-            agreements[from].balance = agreements[from].balance.sub(amount);
-            agreements[to].balance = agreements[to].balance.add(amount);
+        agreement.owner = to;
 
-            agreements[to].agreementHash = agreements[from].agreementHash;
-            agreements[to].valuationCap = agreements[from].valuationCap;
-            agreements[to].discount = agreements[from].discount;
-        }
+        agreementOwners[to] = agreementOwners[from];
+        agreementOwners[from] = 0x0;
 
         emit Transfer(from, to, amount);
     }
@@ -129,12 +131,13 @@ contract PreToken is Restricted {
     // [index in allAgreements, account address as uint, balance, agreementHash as uint,
     //          discount as uint, valuationCap as uint ]
     function getAllAgreements(uint offset) external view returns(uint[6][CHUNK_SIZE] agreementsResult) {
+
         for (uint8 i = 0; i < CHUNK_SIZE && i + offset < allAgreements.length; i++) {
-            address agreementAccount = allAgreements[i + offset];
-            Agreement storage agreement = agreements[agreementAccount];
-            agreementsResult[i] = [ i + offset, uint(agreementAccount), agreement.balance,
-                uint(agreement.agreementHash), uint(agreement.discount), uint(agreement.valuationCap)];
+            bytes32 agreementHash = allAgreements[i + offset];
+            Agreement storage agreement = agreements[agreementHash];
+
+            agreementsResult[i] = [ i + offset, uint(agreement.owner), agreement.balance,
+                uint(agreementHash), uint(agreement.discount), uint(agreement.valuationCap)];
         }
     }
-
 }
--- test/preTokenTransfer.js
+++ test/preTokenTransfer.js
@@ -9,7 +9,6 @@ const initialIssue = 1000000;
 contract("PreToken transfer", accounts => {
     before(async () => {
         preToken = PreToken.at(PreToken.address);
-        // account 0 used for issue/transfer tests
         agreement = {
             owner: accounts[3],
             hash: "0x36517e28afd52e6a9fc53d6922833c67b02e339943d737f1abefa877ff69b68a",
@@ -18,7 +17,7 @@ contract("PreToken transfer", accounts => {
         };
         await preToken.addAgreement(agreement.owner, agreement.hash, agreement.discount, agreement.cap);
 
-        await preToken.issueTo(agreement.owner, initialIssue);
+        await preToken.issueTo(agreement.hash, initialIssue);
     });
 
     beforeEach(async function() {
@@ -31,88 +30,49 @@ contract("PreToken transfer", accounts => {
 
     it("should transfer to an account which has no agreement yet", async function() {
         const to = accounts[4];
-        const [supplyBefore, ownerBalBefore] = await Promise.all([
-            preToken.totalSupply(),
-            preToken.balanceOf(agreement.owner),
-            preToken.balanceOf(to)
-        ]);
+        const supplyBefore = await preToken.totalSupply();
 
         const tx = await preToken.transfer(to, initialIssue, { from: agreement.owner });
         testHelpers.logGasUse(this, tx, "PreToken.transfer");
 
-        const [supplyAfter, agreementAfter, ownerBalAfter, toBalAfter] = await Promise.all([
+        const [
+            supplyAfter,
+            agreementAfter,
+            fromAgreementOwnerHashAfter,
+            toAgreementOwnerHashAfter,
+            fromBalAfter,
+            toBalAfter
+        ] = await Promise.all([
             preToken.totalSupply(),
-            preToken.agreements(to),
+            preToken.agreements(agreement.hash),
+            preToken.agreementOwners(agreement.owner),
+            preToken.agreementOwners(to),
             preToken.balanceOf(agreement.owner),
             preToken.balanceOf(to),
             testHelpers.assertEvent(preToken, "Transfer", { from: agreement.owner, to, amount: initialIssue })
         ]);
 
         assert.equal(supplyAfter.toString(), supplyBefore.toString());
-        assert.equal(ownerBalAfter.toString(), ownerBalBefore.sub(initialIssue).toString());
         assert.equal(toBalAfter.toNumber(), initialIssue);
+        assert.equal(fromBalAfter.toString(), "0");
 
-        assert.equal(agreementAfter[0].toNumber(), initialIssue);
-        assert.equal(agreementAfter[1], agreement.hash);
+        assert.equal(fromAgreementOwnerHashAfter, "0x0000000000000000000000000000000000000000000000000000000000000000");
+        assert.equal(toAgreementOwnerHashAfter, agreement.hash);
+
+        assert.equal(agreementAfter[0], to);
+        assert.equal(agreementAfter[1].toNumber(), initialIssue);
         assert.equal(agreementAfter[2].toNumber(), agreement.discount);
         assert.equal(agreementAfter[3].toNumber(), agreement.cap);
     });
 
-    it("should transfer to an account which has the same agreement", async function() {
-        const to = accounts[4];
-        const transferAmount2 = 100;
-
-        // first transfer to an account w/o agreement ("copy" agreement)
-        const tx1 = await preToken.transfer(to, initialIssue, { from: agreement.owner });
-        testHelpers.logGasUse(this, tx1, "PreToken.transfer");
-
-        // issue a bit more to the from account to have something to transfer
-        const tx2 = await preToken.issueTo(agreement.owner, transferAmount2);
-        testHelpers.logGasUse(this, tx2, "PreToken.issueTo");
-
-        // second transfer to same account
-        const tx3 = await preToken.transfer(to, transferAmount2, { from: agreement.owner });
-        testHelpers.logGasUse(this, tx3, "PreToken.transfer");
-
-        await testHelpers.assertEvent(preToken, "Transfer", { from: agreement.owner, to, amount: transferAmount2 });
-    });
-
-    it("should tansfer 0 amount (voting) to an account w/o agreement", async function() {
+    it("should NOT transfer 0 amount", async function() {
         const to = accounts[4];
         const amount = 0;
 
-        const tx = await preToken.transfer(to, amount, { from: agreement.owner });
-        testHelpers.logGasUse(this, tx, "PreToken.transfer");
-
-        const [agreementAfter] = await Promise.all([
-            preToken.agreements(to),
-            testHelpers.assertEvent(preToken, "Transfer", { from: agreement.owner, to, amount })
-        ]);
-
-        assert.equal(agreementAfter[0].toString(), amount.toString());
-        assert.equal(agreementAfter[1], "0x0000000000000000000000000000000000000000000000000000000000000000");
-        assert.equal(agreementAfter[2].toNumber(), "0");
-        assert.equal(agreementAfter[3].toNumber(), "0");
+        await testHelpers.expectThrow(preToken.transfer(to, amount, { from: agreement.owner }));
     });
 
-    it("should tansfer 0 amount (voting) to an account with a different agreement", async function() {
-        const amount = 0;
-        const agreement2 = {
-            owner: accounts[4],
-            hash: "0x36517e28afd52e6a9fc53d6922833c67b02e339943d737f1abefa877ff69b68a",
-            discount: 700000,
-            cap: 15000000
-        };
-
-        await preToken.addAgreement(agreement2.owner, agreement2.hash, agreement2.discount, agreement2.cap);
-
-        const tx2 = await preToken.transfer(agreement2.owner, amount, { from: agreement.owner });
-        testHelpers.logGasUse(this, tx2, "PreToken.transfer");
-
-        await testHelpers.assertEvent(preToken, "Transfer", { from: agreement.owner, to: agreement2.owner, amount });
-    });
-
-    it("should NOT transfer to an account which has a different agreement", async function() {
+    it("should NOT transfer to an account which has an agreement", async function() {
         const agreement2 = {
             owner: accounts[4],
             hash: "0x66517e28afd52e6a9fc53d6922833c67b02e339943d737f1abefa877ff69b68a",
@@ -151,38 +111,34 @@ contract("PreToken transfer", accounts => {
 
     it("should transferFrom", async function() {
         const to = accounts[4];
-        const from = agreement.owner;
-        const [supplyBefore, ownerBalBefore] = await Promise.all([
-            preToken.totalSupply(),
-            preToken.balanceOf(agreement.owner),
-            preToken.balanceOf(to)
-        ]);
+        const fromHash = agreement.hash;
+        const supplyBefore = await preToken.totalSupply();
 
-        const tx = await preToken.transferFrom(from, to, initialIssue, { from: accounts[0] });
+        const tx = await preToken.transferAgreement(fromHash, to, { from: accounts[0] });
         testHelpers.logGasUse(this, tx, "PreToken.transfer");
 
         const [supplyAfter, agreementAfter, ownerBalAfter, toBalAfter] = await Promise.all([
             preToken.totalSupply(),
-            preToken.agreements(to),
+            preToken.agreements(agreement.hash),
             preToken.balanceOf(agreement.owner),
             preToken.balanceOf(to),
             testHelpers.assertEvent(preToken, "Transfer", { from: agreement.owner, to, amount: initialIssue })
         ]);
 
         assert.equal(supplyAfter.toString(), supplyBefore.toString());
-        assert.equal(ownerBalAfter.toString(), ownerBalBefore.sub(initialIssue).toString());
+        assert.equal(ownerBalAfter.toNumber(), 0);
         assert.equal(toBalAfter.toNumber(), initialIssue);
 
-        assert.equal(agreementAfter[0].toNumber(), initialIssue);
-        assert.equal(agreementAfter[1], agreement.hash);
+        assert.equal(agreementAfter[0], to);
+        assert.equal(agreementAfter[1].toNumber(), initialIssue);
         assert.equal(agreementAfter[2].toNumber(), agreement.discount);
         assert.equal(agreementAfter[3].toNumber(), agreement.cap);
     });
 
     it("only permitted should transferFrom", async function() {
         const to = accounts[4];
-        const from = agreement.owner;
+        const fromHash = agreement.owner;
 
-        await testHelpers.expectThrow(preToken.transferFrom(from, to, initialIssue, { from: accounts[1] }));
+        await testHelpers.expectThrow(preToken.transferAgreement(fromHash, to, { from: accounts[1] }));
     });
 });
