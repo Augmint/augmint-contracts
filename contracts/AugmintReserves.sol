f6bd567ebc2901d8d7fdac0f10d483428577ceaf
Treer Zoltán <treer@euedge.com>
2018-08-21 15:50:36 +0200

    AugmintReserves is no longer a SystemAccount, add simple migrate function instead of withdraw

--- contracts/AugmintReserves.sol
+++ contracts/AugmintReserves.sol
@@ -12,16 +12,27 @@ import "./generic/SystemAccount.sol";
 import "./interfaces/AugmintTokenInterface.sol";
 
 
-contract AugmintReserves is SystemAccount {
+contract AugmintReserves is Restricted {
+
+    event ReserveMigration(address to, uint weiAmount);
 
     constructor(address permissionGranterContract)
-    public SystemAccount(permissionGranterContract) {} // solhint-disable-line no-empty-blocks
+    public Restricted(permissionGranterContract) {} // solhint-disable-line no-empty-blocks
 
     function () external payable { // solhint-disable-line no-empty-blocks
         // to accept ETH sent into reserve (from defaulted loan's collateral )
     }
 
-    function burn(AugmintTokenInterface augmintToken, uint amount) external restrict("MonetarySupervisor") {
+    function burn(AugmintTokenInterface augmintToken, uint amount)
+    external restrict("MonetarySupervisor") {
         augmintToken.burn(amount);
+    }
+
+    function migrate(address to, uint weiAmount)
+    external restrict("StabilityBoard") {
+        if (weiAmount > 0) {
+            to.transfer(weiAmount);
+        }
+        emit ReserveMigration(to, weiAmount);
     }
 }
