858c7b3bb13b4a81c7489ca3263f3ae9660b92b5
Treer Zoltán <treer@euedge.com>
2018-10-30 17:35:44 +0100

    rinkeby scripts: setup SB signers

--- rinkeby_migrations/4_deploy_redeploy_init.js
+++ rinkeby_migrations/4_deploy_redeploy_init.js
@@ -2,6 +2,7 @@ const Migrations = artifacts.require("./Migrations.sol");
 const Rinkeby_0002_redeployInitAll = artifacts.require("./Rinkeby_0002_redeployInitAll.sol");
 const Rinkeby_0003_migrateLegacy = artifacts.require("./Rinkeby_0003_migrateLegacy.sol");
 const Rinkeby_0004_setupLegacy = artifacts.require("./Rinkeby_0004_setupLegacy.sol");
+const Rinkeby_0005_setupSigners = artifacts.require("./Rinkeby_0005_setupSigners.sol");
 
 const MIGRATION_STEP_NUMBER = 4;
 const MIGRATIONS_ADDRESS = "0xc9a7258b2b1ea36ce735793e4816ad949532c9fd";
@@ -12,11 +13,14 @@ module.exports = function(deployer) {
         // script for init new contracts
         await deployer.deploy(Rinkeby_0002_redeployInitAll);
 
-        // script for migrate legacy contracts
+        // script for migrate legacy contracts (to be run via old proxy!)
         await deployer.deploy(Rinkeby_0003_migrateLegacy);
 
         // script for setup legacy contract dependencies
         await deployer.deploy(Rinkeby_0004_setupLegacy);
+
+        // script for setup StabilityBoard signers
+        await deployer.deploy(Rinkeby_0005_setupSigners);
 
         console.log("Done with migration step " + MIGRATION_STEP_NUMBER);
         await Migrations.at(MIGRATIONS_ADDRESS).setCompleted(MIGRATION_STEP_NUMBER);
