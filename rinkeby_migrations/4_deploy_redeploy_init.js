7a41f6bd2a4112fdf9a1aea54738810b31a1c522
Treer Zoltán <treer@euedge.com>
2018-10-30 17:23:28 +0100

    rinkeby redeploy legacy setup script

--- rinkeby_migrations/4_deploy_redeploy_init.js
+++ rinkeby_migrations/4_deploy_redeploy_init.js
@@ -1,6 +1,7 @@
 const Migrations = artifacts.require("./Migrations.sol");
 const Rinkeby_0002_redeployInitAll = artifacts.require("./Rinkeby_0002_redeployInitAll.sol");
 const Rinkeby_0003_migrateLegacy = artifacts.require("./Rinkeby_0003_migrateLegacy.sol");
+const Rinkeby_0004_setupLegacy = artifacts.require("./Rinkeby_0004_setupLegacy.sol");
 
 const MIGRATION_STEP_NUMBER = 4;
 const MIGRATIONS_ADDRESS = "0xc9a7258b2b1ea36ce735793e4816ad949532c9fd";
@@ -13,6 +14,9 @@ module.exports = function(deployer) {
 
         // script for migrate legacy contracts
         await deployer.deploy(Rinkeby_0003_migrateLegacy);
+
+        // script for setup legacy contract dependencies
+        await deployer.deploy(Rinkeby_0004_setupLegacy);
 
         console.log("Done with migration step " + MIGRATION_STEP_NUMBER);
         await Migrations.at(MIGRATIONS_ADDRESS).setCompleted(MIGRATION_STEP_NUMBER);
