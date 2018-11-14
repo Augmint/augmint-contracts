8f4b2037fc720532c7f7fb70b754546a6f708134
Treer Zoltán <treer@euedge.com>
2018-11-14 16:51:58 +0100

    add pretoken signer script to script deploy script

--- mainnet_migrations/31_deploy_all_scripts.js
+++ mainnet_migrations/31_deploy_all_scripts.js
@@ -3,6 +3,7 @@ const Main0014_initNewContracts = artifacts.require("./Main0014_initNewContracts
 const Main0015_migrateLegacyContracts = artifacts.require("./Main0015_migrateLegacyContracts.sol");
 const Main0016_setupLegacy = artifacts.require("./Main0016_setupLegacy.sol");
 const Main0017_postDeploySetup = artifacts.require("./Main0017_postDeploySetup.sol");
+const Main0018_preTokenSigners = artifacts.require("./Main0018_preTokenSigners.sol");
 
 const MIGRATION_STEP_NUMBER = 31;
 const MIGRATIONS_ADDRESS = "0xf01C976E9189BC9ba68Eda0f1Dc9d94b243C78dC";
@@ -21,6 +22,9 @@ module.exports = function(deployer) {
 
         // script for post deploy setup
         await deployer.deploy(Main0017_postDeploySetup);
+
+        // script for pretoken signers
+        await deployer.deploy(Main0018_preTokenSigners);
 
         console.log("Done with migration step " + MIGRATION_STEP_NUMBER);
         await Migrations.at(MIGRATIONS_ADDRESS).setCompleted(MIGRATION_STEP_NUMBER);
