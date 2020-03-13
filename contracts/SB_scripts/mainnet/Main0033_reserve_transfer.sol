fee04e5288f48d6e43f94842fedc878f6fdd0912
Zoltán Treer <treer@decent.org>
2020-03-13 12:18:27 +0100

    script: reserve transfer - fix typo

--- contracts/SB_scripts/mainnet/Main0033_reserve_transfer.sol
+++ contracts/SB_scripts/mainnet/Main0033_reserve_transfer.sol
@@ -9,7 +9,7 @@ contract Main0033_reserve_transfer {
     AugmintReserves public constant AUGMINT_RESERVES = AugmintReserves(0x65F30f8DD20c707C1938CcAd7416c7381E6eB9C8);
     address public constant TARGET_ADDRESS = 0x53DBF6E8fe46307C7536eAbb0D90CADA3e732716;
 
-    function execute(Main0032_reserve_transfer /* self, not used */) external {
+    function execute(Main0033_reserve_transfer /* self, not used */) external {
         // called via StabilityBoardProxy
         require(address(this) == address(STABILITY_BOARD_PROXY), "only execute via StabilityBoardProxy");
 
